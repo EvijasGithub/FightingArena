fcea41d70aff9fd6e58159d2430b637eb5d1c220
jguzovs <108758048+jguzovs@users.noreply.github.com>
2022-07-14 13:53:47 +0300

    new_feature_3
    implemented helper methods

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,9 @@ public class Main {
 
         //Generating a random weapon for Gladiator
         Random random=new Random();
-        System.out.println("Welcome to Fighting Arena! Your opponent is Gladiator.");
+        HelpMethods.clearConsole();
+        HelpMethods.printHeading("Welcome to Fighting Arena! Your opponent is Gladiator.");
+        HelpMethods.anythingToContinue();
         boolean finished = false;
 
         //User can magically come back to life three times
@@ -28,53 +30,82 @@ public class Main {
             //Logic, going through all the possible combinations of weapons
             if (input.equalsIgnoreCase("giant hammer") && computersChoice.equals("giant hammer")) {
                 System.out.println("Gladiator slammed you in the chest. You died.☠");
+                HelpMethods.printSeparator(20);
                 //Every time user dies, one life comes off.
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("giant hammer") && computersChoice.equals("spear")) {
                 System.out.println("Gladiator threw his spear right through your heart.You died.☠");
+                HelpMethods.printSeparator(20);
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("giant hammer") && computersChoice.equals("sword")) {
                 System.out.println("You broke his leg with your hammer. Gladiator passed out. You win!");
+                HelpMethods.printSeparator(20);
                 System.out.println("You have "+lives+" lives left.");
                 computerLives--;
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             }else if (input.equalsIgnoreCase("sword") && computersChoice.equals("sword")) {
                 System.out.println("Gladiator slashed you with his sword. You died.☠");
+                HelpMethods.printSeparator(20);
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("sword") && computersChoice.equals("spear")) {
                 System.out.println("You put your sword trough his stomach. You win!");
+                HelpMethods.printSeparator(20);
                 System.out.println("You have "+lives+" lives left.");
                 computerLives--;
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("sword") && computersChoice.equals("giant hammer")) {
                 System.out.println("He broke your back with his hammer. You died.☠");
+                HelpMethods.printSeparator(20);
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             }else if (input.equalsIgnoreCase("spear") && computersChoice.equals("spear")) {
                 System.out.println("Gladiator hit you in the back with his spear. You died.☠");
+                HelpMethods.printSeparator(20);
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("spear") &&computersChoice.equals ("sword")) {
                 System.out.println("You hit him in the face with your spear.You win!");
+                HelpMethods.printSeparator(20);
                 System.out.println("You have "+lives+" lives left.");
                 computerLives--;
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             } else if (input.equalsIgnoreCase("spear") && computersChoice.equals("giant hammer")) {
                 System.out.println("He smashed your hand and you bled out.☠");
+                HelpMethods.printSeparator(20);
                 lives--;
                 System.out.println("You have "+lives+" lives left.");
                 System.out.println("Gladiator have "+computerLives+" lives left.");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             }else{
                 System.out.println("Error");
+                HelpMethods.anythingToContinue();
+                HelpMethods.clearConsole();
             }
             //Game ends when user runs out of lives
             if (lives == 0) {
